50fd20af9379d7bd4772ffd47f6d62796eb2e23c
Kauê <kauevecchia@hotmail.com>
2025-04-06 16:12:42 -0300

    feat: add basic structure to Profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+export function Profile() {
+  return (
+    <section>
+      <img src="" alt="" />
+      <div>
+        <div>
+          <h1></h1>
+          <a href=""></a>
+        </div>
+        <p></p>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <span></span>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <span></span>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
