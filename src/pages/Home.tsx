e8b2777c2593571899095a8cd151f7f3bc45bbc3
Kauê <kauevecchia@hotmail.com>
2025-04-07 17:40:05 -0300

    feat: create Issue interface

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,17 @@
 import { Profile } from "../components/Profile";
 
+export interface Issue {
+  id: number;
+  title: string;
+  body: string;
+  comments: number;
+  html_url: string;
+  created_at: string;
+  user: {
+    login: string;
+  };
+}
+
 export function Home() {
   return (
     <div className="max-w-5xl">
