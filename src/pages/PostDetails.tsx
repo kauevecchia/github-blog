b1dc2ddfbcda028c9ddfd8e2bbcf4d737fad811c
Kauê <kauevecchia@hotmail.com>
2025-04-08 18:28:11 -0300

    style: add styling to PostDetails page

--- src/pages/PostDetails.tsx
+++ src/pages/PostDetails.tsx
@@ -2,6 +2,11 @@ import { useParams } from "react-router-dom";
 import { Issue } from "./Home";
 import { useEffect, useState } from "react";
 import { api } from "../lib/axios";
+import arrowLeft from "../assets/arrow-left.svg";
+import linkSvg from "../assets/link-svg.svg";
+import githubSvg from "../assets/github.svg";
+import calendarSvg from "../assets/calendar.svg";
+import commentSvg from "../assets/comment.svg";
 
 export function PostDetails() {
   const [postInfo, setPostInfo] = useState<Issue | null>(null);
@@ -21,43 +26,49 @@ export function PostDetails() {
   }
 
   return (
-    <section>
-      <div>
-        <div>
-          <div>
-            <div>
-              <button>
-                <img src={} />
-                <span>VOLTAR</span>
+    <section className="-mt-12 max-w-5xl">
+      <div className="bg-base-profile rounded-lg p-8">
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between">
+              <button className="hover:border-blue flex cursor-pointer items-center justify-center gap-2 border-b-2 border-transparent">
+                <img src={arrowLeft} />
+                <span className="text-blue text-xs">VOLTAR</span>
               </button>
-              <a>
-                <span>VER NO GITHUB</span>
-                <img src={} />
+              <a
+                href={}
+                className="hover:border-blue flex cursor-pointer items-center justify-center gap-2 border-b-2 border-transparent"
+                target="_blank"
+              >
+                <span className="text-blue text-xs">VER NO GITHUB</span>
+                <img src={linkSvg} alt="" />
               </a>
             </div>
           </div>
 
-          <div>
-            <h1>{}</h1>
-            <div>
-              <div>
-                <img src={} />
-                <span>{}</span>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-base-text text-2xl font-bold">{}</h1>
+            <div className="flex gap-6">
+              <div className="flex items-center justify-center gap-2">
+                <img src={githubSvg} alt="" className="h-5 w-5" />
+                <span className="text-base-subtitle">{}</span>
               </div>
-              <div>
-                <img src={} />
-                <span>{}</span>
+              <div className="flex items-center justify-center gap-2">
+                <img src={calendarSvg} alt="" className="h-5 w-5" />
+                <span className="text-base-subtitle">{}</span>
               </div>
-              <div>
-                <img src={} />
-                <span>{} comentários</span>
+              <div className="flex items-center justify-center gap-2">
+                <img src={commentSvg} alt="" className="h-5 w-5" />
+                <span className="text-base-subtitle">{} comentários</span>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div>{}</div>
+      <div className="bg-base-background rounded-b-lg px-8 py-10 shadow-md shadow-slate-800">
+        {}
+      </div>
     </section>
   );
 }
