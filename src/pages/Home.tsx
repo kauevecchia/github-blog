5fbe15fec8f1769cf53d6b02d11a1e58ac8972e9
Kauê <kauevecchia@hotmail.com>
2025-04-08 19:00:43 -0300

    feat: conditionally render Home page content and loading component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ import { searchFormSchema } from "../schemas/searchForm";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loading } from "../components/Loading";
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
@@ -49,49 +50,55 @@ export function Home() {
   }, []);
 
   return (
-    <div className="flex max-w-5xl flex-col gap-16">
-      <Profile />
+    <>
+      {issuesInfo ? (
+        <div className="flex max-w-5xl flex-col gap-16">
+          <Profile />
 
-      <div className="flex flex-col gap-10">
-        <div className="flex flex-col gap-3">
-          <div className="flex items-center justify-between">
-            <span className="text-base-subtitle text-lg font-bold">
-              Publicações
-            </span>
-            <span className="text-base-span text-sm">
-              {issuesInfo?.length} publicações
-            </span>
+          <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-3">
+              <div className="flex items-center justify-between">
+                <span className="text-base-subtitle text-lg font-bold">
+                  Publicações
+                </span>
+                <span className="text-base-span text-sm">
+                  {issuesInfo?.length} publicações
+                </span>
+              </div>
+              <form
+                className="flex items-center justify-center gap-2"
+                onSubmit={handleSubmit(handleSearchPosts)}
+              >
+                <input
+                  type="text"
+                  placeholder="Buscar conteúdo"
+                  className="border-base-border placeholder:text-base-label focus:border-blue text-base-text w-full rounded-md border-2 px-4 py-3 focus:outline-none"
+                  {...register("searchInput")}
+                />
+                <button
+                  type="submit"
+                  className="border-base-border hover:border-blue bg-base-input flex cursor-pointer items-center justify-center gap-1 rounded-md border-2 px-4 py-3 text-white transition duration-300"
+                >
+                  Buscar
+                  <MagnifyingGlass className="text-white" />
+                </button>
+              </form>
+            </div>
+
+            <div className="grid grid-cols-2 gap-6">
+              {issuesInfo?.length !== 0 ? (
+                issuesInfo?.map((issue) => <Post issueInfo={issue} />)
+              ) : (
+                <p className="text-base-span text-lg">
+                  Não encontramos resultado para sua pesquisa
+                </p>
+              )}
+            </div>
           </div>
-          <form
-            className="flex items-center justify-center gap-2"
-            onSubmit={handleSubmit(handleSearchPosts)}
-          >
-            <input
-              type="text"
-              placeholder="Buscar conteúdo"
-              className="border-base-border placeholder:text-base-label focus:border-blue text-base-text w-full rounded-md border-2 px-4 py-3 focus:outline-none"
-              {...register("searchInput")}
-            />
-            <button
-              type="submit"
-              className="border-base-border hover:border-blue bg-base-input flex cursor-pointer items-center justify-center gap-1 rounded-md border-2 px-4 py-3 text-white transition duration-300"
-            >
-              Buscar
-              <MagnifyingGlass className="text-white" />
-            </button>
-          </form>
         </div>
-
-        <div className="grid grid-cols-2 gap-6">
-          {issuesInfo?.length !== 0 ? (
-            issuesInfo?.map((issue) => <Post issueInfo={issue} />)
-          ) : (
-            <p className="text-base-span text-lg">
-              Não encontramos resultado para sua pesquisa
-            </p>
-          )}
-        </div>
-      </div>
-    </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 }
